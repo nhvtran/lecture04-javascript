dd556e1fefc3cbcfd36ae3d2a403c736565a0f1f
Nancy Tran <nhvt@uw.edu>
2016-10-17 10:16:57 -0700

    Add classwork

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+'use strict';
+
 /* JavaScript will go here */
+
+console.log("Hello world!");
+
+
+// JavaScript
+function sayHello(name){
+    return "Hello, "+name;
+}
+
+var msg = sayHello("Joel");
+console.log(msg);
+
+
+// Function Practice
+var circle = {cx:50, cy:50, radius:35};
+function getArea(circle) {
+    return (Math.PI*circle.radius*circle.radius);
+}
+console.log(getArea(circle));
